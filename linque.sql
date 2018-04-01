c7c97a377f01413c8bd202bd11fc3c983b229c47
OfficialLinque <37807204+OfficialLinque@users.noreply.github.com>
2018-04-01 15:17:21 +0800

    Add files via upload

---
+++ linque.sql
@@ -0,0 +1,141 @@
+-- phpMyAdmin SQL Dump
+-- version 4.7.4
+-- https://www.phpmyadmin.net/
+--
+-- Host: 127.0.0.1
+<<<<<<< HEAD
+<<<<<<< HEAD
+-- Generation Time: Apr 01, 2018 at 09:09 AM
+=======
+-- Generation Time: Apr 01, 2018 at 08:42 AM
+>>>>>>> ddf7c6c7a6a609e595c4e21673743c492179c35f
+=======
+-- Generation Time: Apr 01, 2018 at 08:42 AM
+>>>>>>> master
+-- Server version: 10.1.26-MariaDB
+-- PHP Version: 7.1.9
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+SET AUTOCOMMIT = 0;
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Database: `linque`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `migrations`
+--
+
+CREATE TABLE `migrations` (
+  `id` int(10) UNSIGNED NOT NULL,
+  `migration` varchar(191) COLLATE utf8mb4_unicode_ci NOT NULL,
+  `batch` int(11) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
+
+--
+-- Dumping data for table `migrations`
+--
+
+INSERT INTO `migrations` (`id`, `migration`, `batch`) VALUES
+(1, '2014_10_12_000000_create_users_table', 1),
+(2, '2014_10_12_100000_create_password_resets_table', 1),
+(3, '2018_03_31_150258_product', 1),
+(4, '2018_03_31_150444_cart', 1),
+(5, '2018_03_31_151046_transaction', 1),
+(6, '2018_03_31_151229_storetype', 1),
+(7, '2018_03_31_151332_producttype', 1),
+(8, '2018_03_31_152007_prodpackprice', 1);
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `password_resets`
+--
+
+CREATE TABLE `password_resets` (
+  `email` varchar(191) COLLATE utf8mb4_unicode_ci NOT NULL,
+  `token` varchar(191) COLLATE utf8mb4_unicode_ci NOT NULL,
+  `created_at` timestamp NULL DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `users`
+--
+
+CREATE TABLE `users` (
+  `id` int(10) UNSIGNED NOT NULL,
+  `fname` varchar(191) COLLATE utf8mb4_unicode_ci NOT NULL,
+  `lname` varchar(191) COLLATE utf8mb4_unicode_ci NOT NULL,
+  `email` varchar(191) COLLATE utf8mb4_unicode_ci NOT NULL,
+  `password` varchar(191) COLLATE utf8mb4_unicode_ci NOT NULL,
+  `strname` varchar(191) COLLATE utf8mb4_unicode_ci NOT NULL,
+  `strtype` int(11) NOT NULL,
+  `strlocation` varchar(191) COLLATE utf8mb4_unicode_ci NOT NULL,
+  `remember_token` varchar(100) COLLATE utf8mb4_unicode_ci DEFAULT NULL,
+  `created_at` timestamp NULL DEFAULT NULL,
+  `updated_at` timestamp NULL DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
+
+--
+-- Dumping data for table `users`
+--
+
+INSERT INTO `users` (`id`, `fname`, `lname`, `email`, `password`, `strname`, `strtype`, `strlocation`, `remember_token`, `created_at`, `updated_at`) VALUES
+(1, 'retailer', 'retailer', 'retailer@gmail.com', '$2y$10$5ZBjtOpjpPLADi3/by5xKO4KP/EmQqJoDomTceaSZpz6kLMvQwjoC', 'retailer', 1, '7.043133300000001,125.57429599999999', '6K1vrFdzl3L5pNEzos06NQKfVYzTHDXmpVVsqjmmolRkcscHTYt2VEPRWBmT', '2018-03-31 21:05:23', '2018-03-31 21:05:23'),
+(2, 'distributor', 'distributor', 'distributor@gmail.com', '$2y$10$N.tIYFNMhf/XrKW.Uu3yK.t3oPwCcoEV.ZvEVPvocguKW90Hf/Ska', 'distributor', 2, '7.0431325,125.5742955', 'ybRkHLfOnKF3oWAsdfrZvnwmfPwKSr4Zf3gBF3XeNPI9f7Y2QmqDziP7nkUD', '2018-03-31 21:06:31', '2018-03-31 21:06:31');
+
+--
+-- Indexes for dumped tables
+--
+
+--
+-- Indexes for table `migrations`
+--
+ALTER TABLE `migrations`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Indexes for table `password_resets`
+--
+ALTER TABLE `password_resets`
+  ADD KEY `password_resets_email_index` (`email`);
+
+--
+-- Indexes for table `users`
+--
+ALTER TABLE `users`
+  ADD PRIMARY KEY (`id`),
+  ADD UNIQUE KEY `users_email_unique` (`email`);
+
+--
+-- AUTO_INCREMENT for dumped tables
+--
+
+--
+-- AUTO_INCREMENT for table `migrations`
+--
+ALTER TABLE `migrations`
+  MODIFY `id` int(10) UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=9;
+
+--
+-- AUTO_INCREMENT for table `users`
+--
+ALTER TABLE `users`
+  MODIFY `id` int(10) UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
