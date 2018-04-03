eca22803c11856aa14b32da961edc43e850b7b77
hacker-abl <ablamparas@gmail.com>
2018-04-03 12:57:17 +0800

    transaction

--- app/Http/Controllers/RetailController.php
+++ app/Http/Controllers/RetailController.php
@@ -44,9 +44,9 @@ class RetailController extends Controller
     }
     public function transaction()
     {
-        // $trans = Transaction::orderBy('id')->groupBy('transid')->get();
+        $trans = Transaction::orderBy('id')->groupBy('transid')->get();
 
-        return view('rtransactions');
+        return view('rtransactions', compact('trans'));
     }
     public function rlocation()
     {
